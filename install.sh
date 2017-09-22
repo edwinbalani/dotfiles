070ce179d16f540a41fff26d8172e06d2943924d
Edwin Balani <git@balani.xyz>
2017-09-22 22:31:22 +0100

    Add basic logging and do code linting (shellcheck)

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "edwinbalani's dotfiles/install.sh starting up..."
 # Exit on error
 set -e
 
@@ -8,17 +9,19 @@ export DOTFILES_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # First, update the dotfiles themselves (if they were git cloned in the first place)
+echo "Updating dotfiles..."
 [ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
 # If zsh is an available shell, get Oh My Zsh (which will automatically chsh to zsh)
 if hash zsh && grep -Fxq "$(which zsh)" /etc/shells
 then
+    echo "Found zsh! Installing oh-my-zsh..."
     if hash curl; then
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     elif hash wget; then
         sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
     else
-        echo WARNING: Oh My Zsh installation could not complete because neither curl nor wget were found on this system.
+        echo "WARNING: Oh My Zsh installation could not complete because neither curl nor wget were found on this system."
     fi
 fi
 
@@ -43,14 +46,16 @@ LINK_CMD="ln $params"
 # Only install vim stuff if vim is installed
 if ( hash vim 2>/dev/null )
 then
+    echo "Vim is installed - installing .vimrc and plugins..."
     # Make necessary (sub)directories
-    mkdir -p $HOME/.vim/colors
-    mkdir -p $HOME/.vim/tmp/{backup,swap}
+    mkdir -p "$HOME/.vim/colors"
+    mkdir -p "$HOME/.vim/tmp/{backup,swap}"
 
     # Link config and colour theme
     $LINK_CMD "$DOTFILES_DIR/.vimrc" "$HOME"
     if ( hash gvim 2>/dev/null )
     then
+        echo "(GVim also detected) - also installing colour scheme and .gvimrc"
         $LINK_CMD "$DOTFILES_DIR/.gvimrc" "$HOME"
         $LINK_CMD "$DOTFILES_DIR/darktooth.vim" "$HOME/.vim/colors/"
     fi
@@ -67,5 +72,5 @@ fi
 # Other config files
 for f in .gitconfig .tmux.conf .gitignore;
 do
-    $LINK_CMD $DOTFILES_DIR/$f $HOME/$f
+    $LINK_CMD "$DOTFILES_DIR/$f" "$HOME/$f"
 done
