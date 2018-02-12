143a310444780da353fb409a14223cfd07edaad6
Edwin Balani <git@balani.xyz>
2018-02-12 14:56:48 +0000

    install/zsh: Add functionality to update antigen
    Previously the installation would skip over an existing antigen
    installation (more specifically, if it found anything at ~/antigen).

--- install/zsh.sh
+++ install/zsh.sh
@@ -1,11 +1,14 @@
 # If zsh is an available shell, get antigen
 if command -v zsh >/dev/null 2>&1 && grep -Fxq "$(which zsh)" /etc/shells
 then
-    echo "Found zsh! Installing antigen..."
-    if [ -e "$HOME/antigen" ]; then
+    if [ -d "$HOME/antigen/.git" ]; then
+        echo "Found a git repo at ~/antigen -- assuming it's antigen.zsh and updating..."
+        git --work-tree="$HOME/antigen" --git-dir="$HOME/antigen/.git" pull origin master
+    elif [ -e "$HOME/antigen" ]; then
         echo "ERROR: $HOME/antigen (the installation location for antigen) already exists."
         echo "       Please move this out of the way before proceeding."
     elif command -v git >/dev/null 2>&1; then
+        echo "Found zsh! Installing antigen..."
         git clone --depth=1 https://github.com/zsh-users/antigen.git "$HOME/antigen"
     else
         echo "ERROR: antigen installation could not complete because git isn't installed on this system."
