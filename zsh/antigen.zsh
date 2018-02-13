695c8099f796c3cefb2a3d07760195cf340ac5b1
Edwin Balani <git@balani.xyz>
2018-02-13 02:20:21 +0000

    Move from 'antigen bundle' lines to bundles block

--- zsh/antigen.zsh
+++ zsh/antigen.zsh
@@ -3,28 +3,30 @@ source "$HOME/antigen/antigen.zsh"
 POWERLEVEL9K_INSTALLATION_PATH=$ANTIGEN_BUNDLES/bhilburn/powerlevel9k
 antigen theme bhilburn/powerlevel9k powerlevel9k
 
-antigen bundle aws
-antigen bundle command-not-found
-antigen bundle common-aliases
-antigen bundle docker
-antigen bundle django
-antigen bundle encode64
-antigen bundle git
-antigen bundle git-extras
-antigen bundle history
-antigen bundle httpie
-antigen bundle jsontools
-antigen bundle perms
-antigen bundle pip
-antigen bundle pylint
-antigen bundle python
-antigen bundle quote
-antigen bundle systemd
-antigen bundle terraform
+antigen bundles <<EOB
+    aws
+    command-not-found
+    common-aliases
+    docker
+    django
+    encode64
+    git
+    git-extras
+    history
+    httpie
+    jsontools
+    perms
+    pip
+    pylint
+    python
+    quote
+    systemd
+    terraform
+    urltools
+    vagrant
+EOB
 if command -v thefuck >/dev/null 2>&1; then
     antigen bundle thefuck
 fi
-antigen bundle urltools
-antigen bundle vagrant
 
 antigen apply
