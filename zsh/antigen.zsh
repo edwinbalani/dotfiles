4652002e15e5dc63b76856b1cb64a0dbccf7f057
Edwin Balani <git@balani.xyz>
2018-02-13 02:25:27 +0000

    zshrc: Reinstate 'antigen use oh-my-zsh' command
    This reverts commit a0c28914bdc426e68c2a08214473843aa0855fe7.

--- zsh/antigen.zsh
+++ zsh/antigen.zsh
@@ -3,6 +3,8 @@ source "$HOME/antigen/antigen.zsh"
 POWERLEVEL9K_INSTALLATION_PATH=$ANTIGEN_BUNDLES/bhilburn/powerlevel9k
 antigen theme bhilburn/powerlevel9k powerlevel9k
 
+antigen use oh-my-zsh
+
 antigen bundles <<EOB
     aws
     command-not-found
