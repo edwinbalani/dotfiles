f323c09e443c813cd6e9eeda3fbe445387ceb89f
Edwin Balani <git@balani.xyz>
2018-10-01 20:51:26 +0100

    bash prompt: remove superfluous function; add time

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -1,11 +1,3 @@
 # simple, two-line bash prompt
 # switches from $ to # when root
-PROMPT_CHAR () {
-    if [ "$(id -u)" -eq 0 ]
-    then
-        printf '#';
-    else
-        printf '$';
-    fi;
-}
-PS1="\n[\u@\h] \w\n$(PROMPT_CHAR) "
+PS1="\n[\u@\h] (\D{%H:%M}) \w \n\$ "
