7e73eed4a87803b7e03f14b21ae95f9910c57ca3
Edwin Balani <git@balani.xyz>
2018-12-26 22:39:16 +0000

    install/zsh.sh: try to update Antigen if found

--- install/zsh.sh
+++ install/zsh.sh
@@ -1,12 +1,15 @@
 # If zsh is an available shell, get antigen
 if is_shell zsh; then
-    if [ -d "$HOME/antigen/.git" ]; then
-        echo "Found a git repo at ~/antigen -- assuming it's antigen.zsh and updating..."
-        git --work-tree="$HOME/antigen" --git-dir="$HOME/antigen/.git" pull origin master
-    elif [ -e "$HOME/antigen" ]; then
-        echo "ERROR: $HOME/antigen (the installation location for antigen) already exists."
-        echo "       Please move this out of the way before proceeding."
-    elif command -v git >/dev/null 2>&1; then
+    if [ -d "$HOME/antigen" ]; then
+        echo "Found a directory at ~/antigen"
+        if command_exists antigen; then
+            echo " -- assuming it's antigen.zsh and updating..."
+            antigen selfupdate
+            antigen update
+        else
+            echo " -- couldn't find the 'antigen' command to update!  Continuing..."
+        fi
+    elif command_exists git; then
         echo "Found zsh! Installing antigen..."
         git clone --depth=1 https://github.com/zsh-users/antigen.git "$HOME/antigen"
     else
