dad35a63b874a9dbb160cf610f7aa77514b33c58
Edwin Balani <git@balani.xyz>
2019-03-25 22:09:44 +0000

    [Breaking change] Remove dot from config filenames
    Symlinks in existing dotfiles installations will need to be updated

--- install/config.sh
+++ install/config.sh
@@ -1,5 +1,5 @@
 # Other config files
-for f in .gitconfig .tmux.conf .gitignore .latexmkrc;
+for f in gitconfig tmux.conf gitignore latexmkrc;
 do
-    $LINK_CMD "$DOTFILES_DIR/config/$f" "$HOME/$f"
+    $LINK_CMD "$DOTFILES_DIR/config/$f" "$HOME/.$f"
 done
