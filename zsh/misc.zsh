0d1ed0213daa6d7130b3f2a8e0918e4976690790
Edwin Balani <git@balani.xyz>
2023-07-03 10:06:56 +0100

    Work around nvm-sh/nvm#3007

--- zsh/misc.zsh
+++ zsh/misc.zsh
@@ -2,3 +2,9 @@ autoload zmv
 
 # opam configuration
 test -r "$HOME/.opam/opam-init/init.zsh" && . "$HOME/.opam/opam-init/init.zsh" >/dev/null 2>/dev/null
+
+# Workaround for https://github.com/nvm-sh/nvm/issues/3007
+# suggested in https://github.com/nvm-sh/nvm/issues/3007#issuecomment-1405002512
+nvm() {
+    sh -c '. "$NVM_DIR/nvm.sh" && nvm "$@"' - "$@"
+}
