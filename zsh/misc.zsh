f541af7c8a424a6adabcdbb48fffc0f0dae18a15
Edwin Balani <git@balani.xyz>
2024-09-04 23:21:52 +0100

    Don't use zsh-nvm plugin; just load it directly

--- zsh/misc.zsh
+++ zsh/misc.zsh
@@ -5,6 +5,9 @@ test -r "$HOME/.opam/opam-init/init.zsh" && . "$HOME/.opam/opam-init/init.zsh" >
 
 # Workaround for https://github.com/nvm-sh/nvm/issues/3007
 # suggested in https://github.com/nvm-sh/nvm/issues/3007#issuecomment-1405002512
-nvm() {
-    sh -c '. "$NVM_DIR/nvm.sh" && nvm "$@"' - "$@"
-}
+export NVM_DIR="$HOME/.nvm"
+. "$NVM_DIR/nvm.sh"
+
+typeset -g HYPHEN_INSENSITIVE="true"
+typeset -g COMPLETION_WAITING_DOTS="true"
+typeset -g HIST_STAMPS="yyyy-mm-dd"
