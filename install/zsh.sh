1ced8b2f319a90f32d35d59d9a651f819db478ed
Edwin Balani <git@balani.xyz>
2024-09-23 18:51:17 +0100

    switch from antigen to antidote in install and zshrc

--- install/zsh.sh
+++ install/zsh.sh
@@ -1,19 +1,8 @@
-# If zsh is an available shell, get antigen
+# If zsh is an available shell, get antidote for plugin management
 if is_shell zsh; then
-    if [ -d "$HOME/antigen" ]; then
-        echo "Found a directory at ~/antigen"
-        if command_exists antigen; then
-            echo " -- assuming it's antigen.zsh and updating..."
-            antigen selfupdate
-            antigen update
-        else
-            echo " -- couldn't find the 'antigen' command to update!  Continuing..."
-        fi
-    elif command_exists git; then
-        echo "Found zsh! Installing antigen..."
-        git clone --depth=1 https://github.com/zsh-users/antigen.git "$HOME/antigen"
-    else
-        echo "ERROR: antigen installation could not complete because git isn't installed on this system."
+    if [ ! -d "$HOME/.antidote" ]; then
+        echo "Installing antidote..."
+        git clone --depth=1  https://github.com/mattmc3/antidote.git "$HOME/.antidote"
     fi
     $LINK_CMD "$DOTFILES_DIR/zsh/zshrc" "$HOME/.zshrc"
 fi
