314c8a52e3ba701d03431b2b6401d580c9b1290f
Edwin Balani <git@balani.xyz>
2024-09-23 18:52:59 +0100

    install: misc utilities cleanup; don't install gibo for now
    gibo is a compiled Go binary rather than a shell script nowadays.  I'll
    probably write an asdf plugin for it instead at some point (doesn't seem
    to be one already).

--- install/utilities.sh
+++ install/utilities.sh
@@ -1,11 +1,11 @@
 # A generic function for installing utilities that place executable script(s)
-# in the root of their Git repository
+# in the root of their Git repository or under a bin/ subdirectory
 install_plugin() {
     dest_dir="$HOME/.local/utilities/$1"
     if [ -d "$dest_dir" ]; then
         if [ -d "$dest_dir/.git" ]; then
             echo "Already found $1 - updating..."
-            git --work-tree="$dest_dir" --git-dir="$dest_dir/.git" pull origin master
+            git -C "$dest_dir" pull origin
         else
             echo "WARNING: Already found $1 in $dest_dir - can't update (didn't detect a git repo)"
         fi
@@ -17,8 +17,13 @@ install_plugin() {
 }
 
 install_plugin asdf "https://github.com/asdf-vm/asdf.git"
-install_plugin gibo "https://github.com/simonwhitaker/gibo.git"
-install_plugin git-fire "https://github.com/edwinbalani/git-fire.git"
+if [ ! -e "$HOME/.asdf" ]; then
+    $LINK_CMD "$HOME/.local/utilities/asdf" "$HOME/.asdf"
+fi
+
+# TODO asdf plugin for gibo
+#install_plugin gibo "https://github.com/simonwhitaker/gibo.git"
+
 if command_exists zathura && command_exists pandoc && (command_exists vim || command_exists nvim); then
     install_plugin notes "https://github.com/edwinbalani/notes.git"
 fi
