af491423f12f5df9b26344cb884b1ee83d0b6bf5
Edwin Balani <git@balani.xyz>
2024-09-29 00:25:26 +0100

    bash: add date and seconds to prompt; make bashrc install idempotent

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -1,3 +1,3 @@
 # simple, two-line bash prompt
 # switches from $ to # when root
-PS1="\n[\u@\h] (\D{%H:%M}) \w \n\$ "
+PS1="\n[\u@\h] (\D{%Y-%m-%d %H:%M:%S}) \w \n\$ "
--- install/bash.sh
+++ install/bash.sh
@@ -1,7 +1,8 @@
 # If bash is an available shell, install a couple of basic things
 # Not much here, since I tend to use zsh
-if is_shell bash; then
-    echo "Found bash! Injecting a simple bashrc hook..."
-    # We don't overwrite the existing bashrc, since there's often some stuff there already
-    echo "source $DOTFILES_DIR/bash/bashrc" >> "$HOME/.bashrc"
+# We don't overwrite the existing bashrc, since there's often some stuff there already
+bash_source_line="source $DOTFILES_DIR/bash/bashrc"
+if is_shell bash && ! grep -q "^$bash_source_line$" "$HOME/.bashrc"; then
+    echo "Hooking our bashrc into ~/.bashrc..."
+    echo "$bash_source_line" >> "$HOME/.bashrc"
 fi
