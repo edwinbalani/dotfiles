302ff0c8d82df6581d71eb6585cb80df02a234af
Edwin Balani <git@balani.xyz>
2024-09-29 00:24:43 +0100

    zsh: only source nvm.sh conditionally

--- zsh/misc.zsh
+++ zsh/misc.zsh
@@ -6,7 +6,7 @@ test -r "$HOME/.opam/opam-init/init.zsh" && . "$HOME/.opam/opam-init/init.zsh" >
 # Workaround for https://github.com/nvm-sh/nvm/issues/3007
 # suggested in https://github.com/nvm-sh/nvm/issues/3007#issuecomment-1405002512
 export NVM_DIR="$HOME/.nvm"
-. "$NVM_DIR/nvm.sh"
+[[ -e "$NVM_DIR" ]] && . "$NVM_DIR/nvm.sh"
 
 typeset -g HYPHEN_INSENSITIVE="true"
 typeset -g COMPLETION_WAITING_DOTS="true"
