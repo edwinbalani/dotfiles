9a1bd48bd9055a9f7b375db4a1d996c557644bfc
Edwin Balani <git@balani.xyz>
2024-09-29 00:27:23 +0100

    install/zsh: remember to symlink zsh_plugins.txt!

--- install/zsh.sh
+++ install/zsh.sh
@@ -5,6 +5,7 @@ if is_shell zsh; then
         git clone --depth=1  https://github.com/mattmc3/antidote.git "$HOME/.antidote"
     fi
     $LINK_CMD "$DOTFILES_DIR/zsh/zshrc" "$HOME/.zshrc"
+    $LINK_CMD "$DOTFILES_DIR/zsh/zsh_plugins.txt" "$HOME/.zsh_plugins.txt"
 fi
 
 # Directory for random local fpath additions
